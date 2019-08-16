6134c215dde7106e8903080d3cec9d52dedc1528
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-08-17 02:33:49 +0530

    added security, login & logout

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 # Create your views here.
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -13,7 +15,7 @@ def post_detail(request , pk):
     post = get_object_or_404(Post , pk=pk)
     return render(request  , 'blog/post_detail.html' , {'post' : post})
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,7 +31,7 @@ def post_new(request):
 
     return render(request , 'blog/post_edit.html' , {'form' : form})
 
-
+@login_required
 def post_edit(request  , pk):
     post = get_object_or_404(Post , pk=pk)
     if request.method == "POST":
@@ -46,11 +48,12 @@ def post_edit(request  , pk):
 
     return render(request , 'blog/post_edit.html' , {'form' : form})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request , 'blog/post_draft_list.html' , {'posts' : posts})
 
-
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
@@ -61,7 +64,7 @@ def publish(self):
     self.published_date = timezone.now()
     self.save()
 
-
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
